8ea61cdcf086ff8b039d20e152f6ba2ebfe24752
anithakiran <anitha1995d@gmail.com>
2024-07-20 14:23:09 +0530

    terraform-expense-dev

---
+++ 02-sg/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "5.48.0"
+        }
+    }
+backend "s3" {
+
+    bucket = "terraform-s3-buckets-ec2" 
+    key = "expense-dev-sg"
+    region = "us-east-1"
+    dynamodb_table = "daws78s-lock"
+ }
+}
+provider "aws" {
+    region = "us-east-1"
+}
+
