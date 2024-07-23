03a35a0b41a9ef9478afd0c08e5560a36d08ecb6
anithakiran <anitha1995d@gmail.com>
2024-07-23 15:39:16 +0530

    terraform-expense

--- 05-apps/expense.sh
+++ 05-apps/expense.sh
@@ -2,6 +2,6 @@
 dnf install ansible -y 
 cd /tmp
 git clone https://github.com/anithakiran/expense-ansible-role.git
-cd expense-ansible-roles
+cd expense-ansible-role
 ansible-playbook main.yaml -e component=backend -e login_password=ExpenseApp1
 ansible-playbook main.yaml -e component=frontend
